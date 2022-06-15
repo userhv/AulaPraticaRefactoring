301e900076dbcb56b2eeb150c5f24df86daa5abb
userhv <heliovictor05@hotmail.com>
2022-06-15 17:39:01 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -35,7 +35,11 @@ public class Rental {
     return thisAmount;
   }
 
-
+  public int getFrequentRenterPoints() {
+    if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) 
+      return 2;
+    return 1;
+  }
 
 
 }
