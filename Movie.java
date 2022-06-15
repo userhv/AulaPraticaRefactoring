8c2431929308f0f151cfb36cd0e2964fe2654d61
userhv <heliovictor05@hotmail.com>
2022-06-15 18:10:29 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,8 +41,6 @@ public class Movie {
    }
 public int getFrequentRenterPoints(int daysRented){
        //Adicionar o trecho de código extraído.
-    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
-      return 2;
-    return 1;
+    return _price.getFrequentRenterPoints(daysRented);
    }
 }
--- Price.java
+++ Price.java
@@ -1,5 +1,10 @@
 public abstract class Price {
-   public abstract int getPriceCode();
-   public abstract double getCharge(int daysRented);
-
+  public abstract int getPriceCode();
+  public abstract double getCharge(int daysRented);
+  public int getFrequentRenterPoints(int daysRented) {
+      //Adicionar o trecho de código extraído.
+    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
+      return 2;
+    return 1;
+   }
 }
